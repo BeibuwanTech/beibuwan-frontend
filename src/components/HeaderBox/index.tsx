a6c8a2df7964c937dec478e0f7c959ce5668a111
Junxing Lu <lujx1994@gmail.com>
2022-02-19 15:56:10 +0800

    feat: 重构首页组件目录

--- src/components/HeaderBox/index.tsx
+++ src/components/HeaderBox/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
-import './index.less';
 
-interface HeaderBoxProps {
-    A?: string
-}
+import styles from './index.less';
+
+interface HeaderBoxProps { }
 
 const HeaderBox: React.FC<HeaderBoxProps> = (props: HeaderBoxProps) => {
-    return <div className="header-box">
-        <div className="header">
-            {/* <div className="header-logo"><a><img src="/style/images/logos/bsublogo.png" alt="" /></a></div> */}
-            <div className="menu-box">
+    return <div className={styles['header-box']}>
+        <div className={styles['header']}>
+            {/* <div className={styles['header-logo']}>
+                <a><img src="/style/images/logos/bsublogo.png" alt="" /></a>
+            </div> */}
+            <div className={styles['menu-box']}>
                 <ul>
-                    <li><a href="/index.html" className="active">首页</a></li>
+                    <li><a href="/index.html" className={styles['active']}>首页</a></li>
                     <li><a href="/polist.html" className="">政策资讯</a></li>
                     <li><a href="/technologyMarket/index.html" className="">技术转移</a></li>
-                    <li><a href="/mall/index.html" className="">易智商城</a></li>
+                    <li><a href="/mall/index.html" className="">科技淘宝</a></li>
                     <li><a href="/alist.html" className="">活动中心</a></li>
                     <li><a href="http://www.castss.com/" className="">科技智库</a></li>
                     <li><a href="/platform.html" className="">平台动态</a></li>
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -5,24 +5,26 @@ export default function IndexPage() {
 
   return (
     <div className={styles['item-list']}>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={styles['item']}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
-        <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={styles['item']}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <div className={classnames(styles['item'], styles['hide'])}></div>
+      <img src='/api/captcha'></img>
+      {/* <embed src='/api/captcha' style={{ display: 'block', width: 160, height: 80, overflow: 'hidden' }}></embed> */}
     </div>
   );
 }
